5f7b4b8268871574508e61d8404cd28ef140d5a9
stefaniabondoc <33728976+robostephanie@users.noreply.github.com>
2021-11-25 23:30:09 +0100

    Lab 3: Working with RxJS Subjects

--- rxjs-subjects/begin/src/app/app.component.ts
+++ rxjs-subjects/begin/src/app/app.component.ts
@@ -1,30 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { SubjectsService } from './core/subjects.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   subscription1Data = [];
   subscription2Data = [];
   subscription3Data = [];
 
-  constructor() {}
+  private sub1 = new Subscription();
+  private sub2 = new Subscription();
+  private sub3 = new Subscription();
+
+  constructor(
+    private readonly subjectsService: SubjectsService
+  ) {}
 
   ngOnInit() {}
 
-  subscribe1() {
+  public ngOnDestroy(): void {
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
+    this.sub3.unsubscribe();
+  }
 
+  subscribe1() {
+    this.sub1 = this.subjectsService.observable$.subscribe(data => this.subscription1Data.push(data));
   }
 
   subscribe2() {
-
+    this.sub2 = this.subjectsService.observable$.subscribe(data => this.subscription2Data.push(data));
   }
 
   subscribe3() {
-
+    this.sub3 = this.subjectsService.observable$.subscribe(data => this.subscription3Data.push(data));
   }
-
-
 }
--- rxjs-subjects/begin/src/app/core/subjects.service.ts
+++ rxjs-subjects/begin/src/app/core/subjects.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +7,8 @@ import { Injectable } from '@angular/core';
 export class SubjectsService {
   
   //Define Subject and Observable properties here
-
+  public observable$: Observable<string>;
+  private subject$: Subject<string>;
 
   constructor() {
     this.init();
@@ -15,10 +16,17 @@ export class SubjectsService {
 
   init() {
     // Create Subject and Observable Here 
-
+    this.subject$ = new BehaviorSubject<string>('');
+    this.observable$ = this.subject$.asObservable();
 
     // Create interval here
+    setInterval(() => {
+      const date = new Date();
 
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+      this.subject$.next(time);
+    }, 3000)
   }
 
 }
